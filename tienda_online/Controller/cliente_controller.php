58fc0649f9ade7b8b9aa6931cacfa41a3632c017
CristinaLunaRico93 <124673540+CristinaLunaRico93@users.noreply.github.com>
2023-03-14 19:30:16 +0000

    He cambiado el cliente_controller.php

--- tienda_online/Controller/cliente_controller.php
+++ tienda_online/Controller/cliente_controller.php
@@ -1,78 +1,129 @@
 <?php
 
-require_once 'Models/cliente.php';
+require_once('../Models/cliente.php');
 
 class ClienteController {
-    
-    // Método para mostrar todos los clientes
-    public function index() {
-        $clientes = Cliente::getAll();
-        require_once 'view/cliente/index.php';
-    }
+  public function __construct(){}
 
-    // Método para mostrar el formulario de creación de un nuevo cliente
-    public function create() {
-        require_once 'view/cliente/create.php';
-    }
+  public function index(){
+    require_once('views/Usuarios/registro.php');
+  }
 
-    // Método para almacenar un nuevo cliente en la base de datos
-    public function store() {
-        $cliente = new Cliente(
-            $_POST['usuario'],
-            $_POST['clave'],
-            $_POST['nombre'],
-            $_POST['apellidos'],
-            $_POST['genero'],
-            $_POST['fechaNacimiento'],
-            $_POST['telefono'],
-            $_POST['email'],
-            $_POST['direccion'],
-            $_POST['tipoIdentificador'],
-            $_POST['identificador']
-        );
-        $cliente->save();
-        header('Location: index.php?controller=cliente&action=index');
-    }
+  public function register(){
+    if(isset($_POST['submit'])){
+            // Recuperar los datos del formulario
+      $usuario = $_POST['usuario'];
+      $clave = password_hash($_POST['clave'], PASSWORD_DEFAULT);
+      $nombre = $_POST['nombre']; 
+      $apellidos = $_POST['apellidos'];
+      $genero = $_POST['genero'];
+      $fechaNacimiento = $_POST['fechaNacimiento'];
+      $telefono = $_POST['telefono'];
+      $email = $_POST['email'];
+      $direccion = $_POST['direccion'];
+      $tipoIdentificador = $_POST['tipoIdentificador'];
+      $identificador = $_POST['identificador'];
 
-    // Método para mostrar los detalles de un cliente específico
-    public function show() {
-        $cliente = Cliente::getById($_GET['id']);
-        require_once 'view/cliente/show.php';
-    }
+            // Validación de datos
+      if(empty($usuario) || empty($clave) || empty($nombre) || empty($apellidos) || empty($email)){
+        $error = "Debe completar todos los campos obligatorios (*)";
+        require_once('../views/Usuarios/registro.php'); 
+      } else {
+                // Crear un objeto de la clase Cliente
+        $cliente = new Cliente($usuario, $clave, $nombre, $apellidos, $genero, $fechaNacimiento, $telefono, $email, $direccion, $tipoIdentificador, $identificador);
 
-    // Método para mostrar el formulario de edición de un cliente
-    public function edit() {
-        $cliente = Cliente::getById($_GET['id']);
-        require_once 'view/cliente/edit.php';
-    }
+                // Guardar el cliente en la base de datos
+        $guardado = $cliente->guardar();
 
-    // Método para actualizar los datos de un cliente en la base de datos
-    public function update() {
-        $cliente = new Cliente(
-            $_POST['usuario'],
-            $_POST['clave'],
-            $_POST['nombre'],
-            $_POST['apellidos'],
-            $_POST['genero'],
-            $_POST['fechaNacimiento'],
-            $_POST['telefono'],
-            $_POST['email'],
-            $_POST['direccion'],
-            $_POST['tipoIdentificador'],
-            $_POST['identificador'],
-            $_POST['id']
-        );
-        $cliente->update();
-        header('Location: index.php?controller=cliente&action=index');
+                // Verificar si el cliente se guardó correctamente
+        if($guardado){
+          $mensaje = "¡El cliente se ha registrado correctamente!";
+          require_once('../views/Usuarios/registro.php');
+        } else {
+          $error = "Error al registrar el cliente. Intente de nuevo más tarde.";
+          require_once('../views/Usuarios/registro.php');
+        }
+      }
+    } else {
+      require_once('../views/Usuarios/registro.php');
     }
+  }
 
-    // Método para eliminar un cliente de la base de datos
-    public function delete() {
-        $cliente = Cliente::getById($_GET['id']);
-        $cliente->delete();
-        header('Location: index.php?controller=cliente&action=index');
+  public function update(){
+    if(isset($_POST['submit'])){
+            // Recuperar los datos del formulario
+      $id = $_POST['id'];
+      $usuario = $_POST['usuario'];
+      $clave = password_hash($_POST['clave'], PASSWORD_DEFAULT);
+      $nombre = $_POST['nombre']; 
+      $apellidos = $_POST['apellidos'];
+      $genero = $_POST['genero'];
+      $fechaNacimiento = $_POST['fechaNacimiento'];
+      $telefono = $_POST['telefono'];
+      $email = $_POST['email'];
+      $direccion = $_POST['direccion'];
+      $tipoIdentificador = $_POST['tipoIdentificador'];
+      $identificador = $_POST['identificador'];
+
+            // Validación de datos
+      if(empty($id) || empty($usuario) || empty($clave) || empty($nombre) || empty($apellidos) || empty($email)){
+        $error = "Debe completar todos los campos obligatorios (*)";
+        require_once('../views/Usuarios/editar.php'); 
+      } else {
+                // Crear un objeto de la clase Cliente
+        $cliente = new Cliente($usuario, $clave, $nombre, $apellidos, $genero, $fechaNacimiento, $telefono, $email, $direccion, $tipoIdentificador, $identificador);
+        $cliente->setId($id);
+
+                // Actualizar el cliente en la base de datos
+        $actualizado = $cliente->actualizar();
+
+                // Verificar si el cliente se actualizó correctamente
+        if($actualizado){
+          $mensaje = "¡El cliente se ha actualizado correctamente!";
+          require_once('../views/Usuarios/editar.php');
+        } else {
+          $error = "Error al actualizar el cliente. Intente de nuevo más tarde.";
+          require_once('../views/Usuarios/editar.php');
+        }
+      }
+    } else {
+// Obtener el ID del cliente a actualizar
+      $id = $_GET['id'];
+ // Obtener los datos del cliente de la base de datos
+      $cliente = Cliente::buscarPorId($id);
+
+        // Verificar si el cliente existe
+      if($cliente){
+            // Mostrar el formulario de edición con los datos del cliente
+        require_once('../views/Usuarios/editar.php');
+      } else {
+            // Mostrar un mensaje de error
+        $error = "El cliente no existe";
+        require_once('../views/Usuarios/error.php');
+      }
     }
-    
+  }
+ public function delete(){
+    if(isset($_POST['id'])){
+        $id = $_POST['id'];
+        $cliente = new Cliente();
+        $cliente->setId($id);
+        $eliminado = $cliente->eliminar();
+
+        if($eliminado){
+            $mensaje = "¡El cliente se ha eliminado correctamente!";
+            require_once('../views/Usuarios/index.php');
+        } else {
+            $error = "Error al eliminar el cliente. Intente de nuevo más tarde.";
+            require_once('../views/Usuarios/index.php');
+        }
+    } else {
+        require_once('../views/Usuarios/index.php');
+    }
+}
+  public function error(){
+    require_once('../views/Usuario/error.php');
+  }
 }
 
 ?>
